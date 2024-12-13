1830ffb288e41d89bbf7049c0d88d5a54a1477a9
unkabas <undercannabis10@gmail.com>
2024-12-12 21:08:12 -0800

    add structure and configs

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import type { PropsWithChildren } from 'react'
+import { Content } from './content/Content'
+import Sidebar from './sidebar/Sidebar'
+
+export function Layout({ children }: PropsWithChildren<unknown>) {
+  return <main>
+            <Sidebar />
+					  <Content>{children}</Content>
+          </main>
+}
+
---
+++ src/components/layout/content/Content.tsx
@@ -0,0 +1,9 @@
+import type { PropsWithChildren } from 'react'
+import { Header } from './header/Header'
+
+export  function Content({ children }: PropsWithChildren<unknown>) {
+  return <div>
+            <Header />
+            <section>{children}</section>
+          </div>
+}
